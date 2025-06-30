59bcf878c6639002c710ec1646837fddfe24edb6
苏青安 <992182040@qq.com>
2025-06-30 15:08:24 +0800

    chore(chore): 完成webman框架的基础搭建
    Closes #2

---
+++ backend/config/route.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+use Webman\Route;
+
+
+
+
+
+
+
+Route::fallback(function () {
+    // 最后兜底
+    return json([
+        'code' => 0,
+        'message' => '别看了哥们，没这个页面',
+        'data' => (object)[]
+    ]);
+});
+
+Route::disableDefaultRoute(); // 关闭默认路由
