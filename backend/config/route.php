b6a9da3f89d0ca194c724ad90117e49a97686acc
苏青安 <992182040@qq.com>
2025-07-01 21:05:34 +0800

    chore(chore): 允许options请求携带Authorization

--- backend/config/route.php
+++ backend/config/route.php
@@ -34,7 +34,7 @@ Route::options('[{path:.+}]', function () {
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Origin' => '*',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Accept-Language',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
         ]);
 });
 
