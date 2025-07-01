dd33a307fce9d93cc31d769d8200022367c7ddda
苏青安 <992182040@qq.com>
2025-07-01 13:32:48 +0800

    feat(chore): 后端完成多语言配置

--- backend/resource/translations/zh_cn/messages.php
+++ backend/resource/translations/zh_cn/messages.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
-    "Success" => "成功"
+    "success" => "成功",
+
+    "User session expired" => "账号登录已过期",
+    "Account does not exist. Please register" => "账号不存在，请注册",
+    "Account has been disabled" => "账号已停用",
+    "Incorrect username or password" => "账号或密码错误",
+
+    "Invalid request parameters" => "请求参数异常",
+    "Signature verification failed" => "签名验证异常",
+    "Expired access token" => "过期的访问",
+    "Data parsing error" => "数据解析异常",
 ];
