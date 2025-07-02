022bef0df4c2310a16dafcb256ecd6b8fa285642
苏青安 <992182040@qq.com>
2025-07-02 16:29:57 +0800

    feat(frontend): 完成个人信息配置页面
    Closes #25

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-100% bg-[#f9fafb] px-4 py-4 dark:bg-[#121212]">
+    <div class="flex flex-wrap gap-4">
+      <ProfileCard :data="profile.profile_card" class="opacity-0 duration-1500" :class="{ 'opacity-100': show[0] }" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import ProfileCard from '@/components/Profile/ProfileCard.vue'
+import { request } from '@/utils/http/request'
+
+defineOptions({
+  name: 'ProfileView',
+})
+
+const emit = defineEmits(['setDefaultValue'])
+emit('setDefaultValue', 'profile')
+
+const profile = ref({})
+const show = ref([false, false, false])
+onMounted(async () => {
+  const res = await request.post('/auth/profile')
+  if (res.code === 0) {
+    profile.value = res.data
+    show.value[0] = true
+    setTimeout(() => (show.value[1] = true), 200)
+    setTimeout(() => (show.value[2] = true), 400)
+  }
+})
+</script>
