4e43afbe8b21338137142c2658818f4c50431f65
zxc7563598 <junjie.he.925@gmail.com>
2025-07-02 11:33:31 +0800

    feat(page): 增加临时的欢迎页
    后期计划以小组件形式展示各个模块的快捷操作/统计图

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <n-card title="首页">
-    这里是首页
-  </n-card>
+  <div class="min-h-100% flex items-center justify-center bg-[#f9fafb] dark:bg-[#121212]">
+    <n-card
+      class="w-auto border border-gray-200 rounded-xl py-5 shadow-lg transition-opacity duration-1500 dark:border-gray-700 dark:shadow-[0_4px_12px_rgba(0,0,0,0.4)]"
+    >
+      欢迎你，我的朋友，目前功能还在开发中，敬请期待
+    </n-card>
+  </div>
 </template>
 
 <script setup>
