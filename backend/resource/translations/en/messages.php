2b909fbf2bb63a546ccb47b4a8a6645dcf2a88bb
zxc7563598 <junjie.he.925@gmail.com>
2025-07-03 11:05:04 +0800

    feat(api): 变更账单监听邮箱配置对信息进行验证

--- backend/resource/translations/en/messages.php
+++ backend/resource/translations/en/messages.php
@@ -18,6 +18,7 @@ return [
     "Account has been disabled" => "Account has been disabled",
     "Incorrect username or password" => "Incorrect username or password",
     "This account is already registered" => "This account is already registered",
+    "Failed to connect to the mailbox. Please check if the information you entered is correct" => "Failed to connect to the mailbox. Please check if the information you entered is correct",
 
     "Invalid request parameters" => "Invalid request parameters",
     "Signature verification failed" => "Signature verification failed",
