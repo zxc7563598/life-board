5807cb8910e766daf436eb71c11cb2be654a1c1b
zxc7563598 <junjie.he.925@gmail.com>
2025-07-03 16:09:21 +0800

    fix(crontab): 调整支付宝/微信账单解析日志信息

--- backend/app/queue/redis/WeChatBillParserHandler.php
+++ backend/app/queue/redis/WeChatBillParserHandler.php
@@ -29,8 +29,9 @@ class WeChatBillParserHandler implements Consumer
             return true;
         };
         $options->onDataParsed = function ($data) use ($user_id) {
-            sublog('队列任务', '微信账单解析', '解析到数据', ['count' => count($data['data'])]);
+            sublog('队列任务', '微信账单解析', '解析到数据', ['count' => count($data['data']), 'user_id' => $user_id]);
             foreach ($data['data'] as $row) {
+                sublog('队列任务', '微信账单解析', '数据', $row);
                 $bill_records = BillRecords::where('user_id', $user_id)->where('trade_no', $row[8])->count();
                 if (!$bill_records) {
                     $bill_records = new BillRecords();
