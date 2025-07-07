f076b7c5e9b1bbc346064049be322b4dab43f020
zxc7563598 <junjie.he.925@gmail.com>
2025-07-07 20:47:46 +0800

    feat(api): 优化用户未配置imap时的接口响应

--- frontend/src/views/Bill.vue
+++ frontend/src/views/Bill.vue
@@ -20,6 +20,16 @@ emit('setDefaultValue', 'bill')
 
 const searchEnums = ref()
 onMounted(async () => {
+  request.post('/bill/has-user-imap-config').then(({ code, data }) => {
+    if (code === 0) {
+      if (!data.billmail_config) {
+        window.$message?.warning('若要使用该功能，请前往「个人配置」「账单监听邮箱配置」中设置监听邮箱')
+      }
+      if (!data.imap_config) {
+        window.$message?.warning('账单监听邮箱配置异常，请前往「个人配置」「账单监听邮箱配置」中重新设置')
+      }
+    }
+  })
   const res = await request.post('/bill/get-bill-search-enums')
   if (res.code === 0) {
     searchEnums.value = res.data
