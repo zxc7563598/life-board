ba27eb9b017e1fa5e4f70a7652e56ee02501a80b
苏青安 <992182040@qq.com>
2025-07-08 16:14:08 +0800

    refactor(frontend): 简单调整首页兼容小组件模式

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="min-h-100% flex items-center justify-center bg-[#f9fafb] dark:bg-[#121212]">
-    <n-card
-      class="w-auto border border-gray-200 rounded-xl py-5 shadow-lg transition-opacity duration-1500 dark:border-gray-700 dark:shadow-[0_4px_12px_rgba(0,0,0,0.4)]"
-    >
-      欢迎你，我的朋友，目前功能还在开发中，敬请期待
-    </n-card>
+  <div class="min-h-100% bg-[#f9fafb] px-4 py-4 dark:bg-[#121212]">
+    <div class="flex flex-wrap gap-4">
+      <TextCard />
+      <TextCard />
+      <IncomeCategoryTop10 />
+    </div>
   </div>
 </template>
 
 <script setup>
+import IncomeCategoryTop10 from '@/components/BillAnalytics/IncomeCategoryTop10.vue'
+import TextCard from '@/components/Home/TextCard.vue'
+
 defineOptions({
   name: 'HomeView',
 })
