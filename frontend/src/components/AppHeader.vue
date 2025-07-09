32a23b0a823578cd40c983dce4dd06ff47cb3e9e
zxc7563598 <junjie.he.925@gmail.com>
2025-07-09 21:07:55 +0800

    feat(frontend): 前端实现仪表盘卡片拖拽与布局保存功能
    Closes #35

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,11 +1,21 @@
 <template>
-  <n-layout-header bordered class="px-3 py-1">
-    LifeBoard · 🗂 你的生活仪表盘
+  <n-layout-header bordered class="flex px-3 py-1">
+    <div>
+      LifeBoard · 🗂 你的生活仪表盘
+    </div>
+    <!-- <div class="ml-a inline-flex">
+      <i class="i-tabler-sun h-full" />
+      <i class="i-tabler-moon h-full" />
+    </div> -->
+    <div class="ml-a inline-flex">
+      <WidgetsSettings />
+    </div>
   </n-layout-header>
 </template>
 
 <script setup>
 import { useMessage } from 'naive-ui'
+import WidgetsSettings from '@/components/WidgetsSettings.vue'
 
 window.$message = useMessage()
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="min-h-100% bg-[#f9fafb] px-4 py-4 dark:bg-[#121212]">
     <div class="flex flex-wrap gap-4">
-      <TextCard />
-      <TextCard />
-      <IncomeCategoryTop10 />
+      <component :is="widgetMap[item]" v-for="(item, index) in widgets" :key="index" />
     </div>
   </div>
 </template>
 
 <script setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import ExpenseCategoryTop10 from '@/components/BillAnalytics/ExpenseCategoryTop10.vue'
 import IncomeCategoryTop10 from '@/components/BillAnalytics/IncomeCategoryTop10.vue'
-import TextCard from '@/components/Home/TextCard.vue'
+import IncomeExpenseOverview from '@/components/BillAnalytics/IncomeExpenseOverview.vue'
+import BillModuleGuide from '@/components/Home/BillModuleGuide.vue'
+import WelcomeMessage from '@/components/Home/WelcomeMessage.vue'
+import eventBus from '@/utils/event-bus'
+import { request } from '@/utils/http/request'
 
 defineOptions({
   name: 'HomeView',
@@ -18,4 +22,31 @@ defineOptions({
 
 const emit = defineEmits(['setDefaultValue'])
 emit('setDefaultValue', 'home')
+
+const widgets = ref([])
+
+const widgetMap = {
+  WelcomeMessage,
+  BillModuleGuide,
+  ExpenseCategoryTop10,
+  IncomeCategoryTop10,
+  IncomeExpenseOverview,
+}
+
+function refreshData() {
+  request.post('/home/get-user-widgets').then(({ code, data }) => {
+    if (code === 0) {
+      widgets.value = data.user_widgets
+    }
+  })
+}
+
+onMounted(() => {
+  refreshData()
+  eventBus.on('refresh-home-view', refreshData)
+})
+
+onBeforeUnmount(() => {
+  eventBus.off('refresh-home-view', refreshData)
+})
 </script>
